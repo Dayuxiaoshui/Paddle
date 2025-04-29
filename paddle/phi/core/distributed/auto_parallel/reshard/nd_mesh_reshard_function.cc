c5ab02f31942bbc0ac85efcd6eb60151e8eab3a8
HydrogenSulfate <490868991@qq.com>
2025-04-29 17:05:42 +0800

    Revert "【Auto-Parallel】optimize nd_mesh_reshard_function in dynamic-auto-para…"
    This reverts commit 715aab2907faf8be0aeaa7cac476940851c039eb.

--- paddle/phi/core/distributed/auto_parallel/reshard/nd_mesh_reshard_function.cc
+++ paddle/phi/core/distributed/auto_parallel/reshard/nd_mesh_reshard_function.cc
@@ -148,8 +148,7 @@ void SameNdMeshReshardFunction::Eval(phi::DeviceContext* dev_ctx,
   // 2. change all the shard status to replicated status
   for (int64_t i = first_diff_axis; i >= 0; --i) {
     int64_t in_mesh_axis = out->dist_attr().dims_mapping()[i];
-    int64_t out_mesh_axis = out_dist_attr_orig.dims_mapping()[i];
-    if (in_mesh_axis != -1 && in_mesh_axis != out_mesh_axis) {
+    if (in_mesh_axis != -1) {
       VLOG(3) << "Step2: in_mesh axis " << in_mesh_axis;
       // 2.1 Calculate the dist_attr after this transform
       TensorDistAttr real_out_dist_attr(out->dist_attr());
@@ -224,9 +223,8 @@ void SameNdMeshReshardFunction::Eval(phi::DeviceContext* dev_ctx,
 
   // 4. Change replicated/partial to shard
   for (int64_t i = first_diff_axis; i >= 0; --i) {
-    int64_t in_mesh_axis = out->dist_attr().dims_mapping()[i];
     int64_t out_mesh_axis = out_dist_attr_orig.dims_mapping()[i];
-    if (in_mesh_axis != out_mesh_axis) {
+    if (out_mesh_axis != -1) {
       const auto& in_partial_status = out->dist_attr().partial_status();
       bool is_partial = in_partial_status.count(out_mesh_axis) != 0;
 
